f979c946acaa697efa512c1b4780c133c1762fb6
Andrew Dansby <andydansby@gmail.com>
2021-08-11 07:04:07 -0400

    Add files via upload

---
+++ CONTENDED/contended.c
@@ -0,0 +1,22 @@
+// using sccz80 1.99c
+
+// assembly files are in ramlow.asm as
+// indicated by ramlow.lst
+//#pragma output CRT_ORG_CODE = 24000
+
+//CONTENDED RAM
+
+
+
+//we are just putting functions here
+
+#include "externs.h"
+#include "variables.h"
+#include "routines.h"
+
+
+
+// do not have main() here at all since we are just compiling to object files
+//we are just putting functions here
+
+//IMPORTANT need to have blank line at very end
